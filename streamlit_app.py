79264b6344efb4965fa45d86a80e5a7d4de884f9
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2022-03-13 20:30:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,22 @@
 import streamlit
-from dashboards import ExportsPage
+#from dashboards import ExportsPage
 import json
 
-page = streamlit.sidebar.selectbox(
-    "Choose Page", ["Summary Stats"])
+#page = streamlit.sidebar.selectbox(
+#    "Choose Page", ["Summary Stats"])
 
-if page == 'Summary Stats':
-    streamlit.header("QueryString")
-    query_params = streamlit.experimental_get_query_params()
-    streamlit.write(query_params)
-    filename = query_params['filename'][0] 
-    streamlit.write(filename)
+#if page == 'Summary Stats':
+#    streamlit.header("QueryString")
+query_params = streamlit.experimental_get_query_params()
+#streamlit.write(query_params)
+filename = query_params['filename'][0] 
+#    streamlit.write(filename)
 
 
 
-    obj = {"filename": filename, "result": 'flower'}
-    streamlit.write(obj)
-    streamlit.write(json.dumps(obj))
+obj = {"filename": filename, "result": 'flower'}
+#    streamlit.write(obj)
+streamlit.write(json.dumps(obj))
 
-    ExportsPage.show_explore_page()
+#    ExportsPage.show_explore_page()
     
